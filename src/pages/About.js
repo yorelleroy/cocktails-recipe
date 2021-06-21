8a32e23c90bf695b6c7440e28c2284f0a216681a
yorelleroy <leroylazala@gmail.com>
2021-06-21 18:37:11 +0900

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo quisquam
+				voluptates enim nisi ipsam incidunt id laboriosam voluptatum nostrum
+				ratione ab, error odit maxime dolores deleniti, omnis alias voluptatibus
+				ut, asperiores harum reiciendis corrupti! Eligendi adipisci illum
+				accusamus itaque cum.
+			</p>
+		</section>
+	);
+};
+
+export default About;
